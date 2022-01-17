5c94c3ec726474dbd4b66fbdb991b1a18b98a103
Rahul <rahul16086@gmail.com>
2022-01-17 12:59:32 +0000

    Added code for adding matches in controller and router

--- controller/matchMaker.js
+++ controller/matchMaker.js
@@ -11,3 +11,36 @@ exports.getMatches = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.addMatch = async (req, res, next) => {
+  try {
+    const userId = req.body.userId;
+    const matchUserId = req.body.matchUserId;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      const error = new Error("User with this id cannot be found.");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    if (user.matches.indexOf(matchUserId) !== -1) {
+      const error = new Error("Already matched");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    user.matches.push(matchUserId);
+    const result = await user.save();
+
+    if (result) {
+      console.log("Matches updated!!");
+      res.json({ message: "Match added" });
+    }
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
--- routes/matchMaker.js
+++ routes/matchMaker.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.get("/matchmaker", matchesController.getMatches);
 
+router.put("/matchmaker/add-match", matchesController.addMatch);
+
 module.exports = router;
