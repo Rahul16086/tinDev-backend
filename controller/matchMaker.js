21cd7753c00df356e670c13b3d5974ad3c9067d7
Rahul <rahul16086@gmail.com>
2022-01-19 10:58:42 +0000

    Added filters and final filters for the matches data and simplified some code

--- controller/matchMaker.js
+++ controller/matchMaker.js
@@ -2,8 +2,50 @@ const User = require("../models/user");
 
 exports.getMatches = async (req, res, next) => {
   try {
+    const userId = req.query.userId;
     const matches = await User.find();
-    res.status(200).json({ matches: matches });
+    const currentUser = [];
+    let filteredMatches;
+    let finalFilter;
+    const leftPaneMatches = [];
+
+    if (matches.length > 0) {
+      filteredMatches = matches.filter((data) => {
+        if (!(data._id.toString() === userId.toString())) {
+          return data;
+        } else {
+          currentUser.push(data);
+        }
+      });
+    }
+
+    if (currentUser.length > 0) {
+      if (currentUser[0].matches?.length > 0) {
+        finalFilter = filteredMatches.filter((data) => {
+          if (currentUser[0].matches?.indexOf(data._id.toString()) === -1) {
+            return data;
+          }
+        });
+
+        currentUser[0].matches.forEach((matchId) => {
+          matches.forEach((match) => {
+            if (matchId.toString() === match._id.toString()) {
+              leftPaneMatches.push({
+                name: match.name,
+                _id: match._id.toString(),
+              });
+            }
+          });
+        });
+      } else {
+        finalFilter = [...filteredMatches];
+      }
+    }
+
+    res.status(200).json({
+      cardData: finalFilter,
+      leftPane: leftPaneMatches,
+    });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
